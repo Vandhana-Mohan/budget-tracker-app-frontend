569edf4740ff90a59c9d411148e363812da33369
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-18 01:18:02 -0400

    edit routes

--- src/App.js
+++ src/App.js
@@ -6,3 +6,23 @@ import FourOFour from "./Pages/FourOFour";
 import Index from "./Pages/Index";
 import New from "./Pages/New";
 import Show from "./Pages/Show";
+
+function App() {
+  return (
+    <div>
+      <h1> Hello </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element=<Home /> />
+          <Route path="/Edit/:index" element=<Edit /> />
+          <Route path="/New" element=<New /> />
+          <Route path="/Show" element=<Show /> />
+          <Route path="/Index" element=<Index /> />
+          <Route path="*" element=<FourOFour /> />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
