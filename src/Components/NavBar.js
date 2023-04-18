19f522c893387da94db42f87e72514739a62c362
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-18 04:24:43 -0400

    add navbar and css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,17 @@
-function NavBar(){
-    return null
+function NavBar() {
+  return (
+    <nav className="bg-gray-700 text-white py-4">
+      <ul className="flex justify-around">
+        <li className="cursor-pointer hover:text-gray-300">
+          View All Transactions
+        </li>
+        <li className="cursor-pointer hover:text-gray-300">New Transaction</li>
+        <li className="cursor-pointer hover:text-gray-300">Edit Transaction</li>
+        <li className="cursor-pointer hover:text-gray-300">
+          Delete Transaction
+        </li>
+      </ul>
+    </nav>
+  );
 }
-export default NavBar
+export default NavBar;
