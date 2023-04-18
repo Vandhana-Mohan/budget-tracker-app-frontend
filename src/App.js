2d98dd9f7f0ce9603e2dd3bce1c5263aa13d5576
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-18 01:08:04 -0400

    add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import "./App.css";
+// DEPENDENCIES
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// COMPONENTS
+import NavBar from "./Components/NavBar";
+
+// PAGES
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Index />} />
+            <Route path="/logs/new" element={<New />} />
+            <Route path="/logs/:index" element={<Show />} />
+            <Route path="/logs/:index/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
