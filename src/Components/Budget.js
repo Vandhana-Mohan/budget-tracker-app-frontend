ec0f5fb5e164e76410245dbbc124e6c0057a5ed4
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-19 07:23:01 -0400

    add css

--- src/Components/Budget.js
+++ src/Components/Budget.js
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 function formatDate(dateString) {
   const date = new Date(dateString);
   const year = date.getFullYear();
-  // const month = String(date.getMonth() + 1).padStart(2, "0");
+  // const month = String(date.getMonth() + 1).padStart(2, "0"); - this is to change month in number
   const day = String(date.getDate()).padStart(2, "0");
   const monthName = date.toLocaleString("default", { month: "long" });
-  return `${monthName} ${day}, ${year}`;
+  // return `${month}-${day}-${year}`; // returns "05-05-2023"
+  return `${monthName} ${day}, ${year}`; // returns "May 05, 2023"
 }
 
 function Budget({ budget, index }) {
   return (
     <tr className="border-b border-gray-200 hover:bg-gray-100">
-      <td className="px-4 py-3">{budget.id}</td>
-      <td className="px-4 py-3">
+      <td className="w-1/12 px-4 py-3">{budget.id}</td>
+      <td className="w-1/12 px-4 py-3">
         <Link
           to={`/budgets/${index}`}
           className="text-blue-600 hover:underline"
@@ -21,12 +22,12 @@ function Budget({ budget, index }) {
           {budget.item_name}
         </Link>
       </td>
-      <td className="px-4 py-3">
+      <td className="w-2/12 px-4 py-3">
         {!budget.isWithdrawal ? "" : "-"} $ {budget.amount}
       </td>
-      <td className="px-4 py-3">{formatDate(budget.date)}</td>
-      <td className="px-4 py-3">{budget.from}</td>
-      <td className="px-4 py-3">{budget.category}</td>
+      <td className="w-1/12 px-4 py-3">{formatDate(budget.date)}</td>
+      <td className="w-1/12 px-4 py-3">{budget.from}</td>
+      <td className="w-1/12 px-4 py-3">{budget.category}</td>
     </tr>
   );
 }
--- src/Components/Budgets.js
+++ src/Components/Budgets.js
@@ -50,18 +50,18 @@ function Budgets() {
         {" "}
         Total Balance: {formattedBalance}
       </h2>
-      <table className="table-fixed border-collapse border-gray-600 border-2">
+      <table className="table-fixed border-collapse border-gray-600 border-2 text-center">
         <thead>
           <tr className="bg-gray-700 text-white">
             <th className="w-1/12 py-2">Id</th>
             <th className="w-2/12 py-2">Item Name</th>
-            <th className="w-1/12 py-2">Amount</th>
+            <th className="w-2/12 py-2">Amount</th>
             <th className="w-2/12 py-2">Date</th>
             <th className="w-2/12 py-2">From</th>
             <th className="w-2/12 py-2">Category</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="text-left">
           {budgets.map((budget, index) => {
             return <Budget key={index} budget={budget} index={index} />;
           })}
