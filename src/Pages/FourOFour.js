51cff6118b55556b1e0815dc05757feb415ac672
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-20 01:34:12 -0400

    edit contact css

--- src/Pages/FourOFour.js
+++ src/Pages/FourOFour.js
@@ -1,6 +1,6 @@
 function FourOFour() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center py-24">
       <h1 className="text-4xl font-bold text-gray-700">Sorry, no page found</h1>
     </div>
   );
